688ff70a009a8bc756a0615e1e12c1e1dda40a80
Cody Shepherd <cls9@babbage.cs.pdx.edu>
2016-10-27 21:49:59 -0700

    Async i/o commplete, or very close

--- scheduled/async.c
+++ scheduled/async.c
@@ -4,8 +4,35 @@
 #include <unistd.h>
 #include <string.h>
 
+/*
+struct aiocb, aio_read, aio_error, aio_return
+EINPROGRESS
+lseek, SEEK_CUR, SEEK_END, SEEK_SET
+memset
+*/
+
+
 ssize_t read_wrap(int fd, void * buf, size_t count) {
 
     struct aiocb * a = malloc(sizeof(struct aiocb));
 
+    off_t offst = lseek(fd, 0, SEEK_CUR);
+    if(offst>=0)
+        a->aio_offset = offst;
+    else
+        a->aio_offset = 0;
+
+    a->aio_fildes = fd;
+    a->aio_buf = buf;
+    a->aio_nbytes = count;
+    a->aio_reqprio = 1; //??
+    a->aio_sigevent.sigev_notify = SIGEV_NONE;
+    //a->aio_lio_opcode  // not needed
+    
+    aio_read(a);
+    while(aio_error(a) == EINPROGRESS)
+        yield();
+    
+    free(a);
+    return aio_return(a);
 }
--- scheduled/main.c
+++ scheduled/main.c
@@ -1,14 +1,25 @@
 //Cody Shepherd
 //CS 533
-#define SLICE
+//#define SLICE
+#define TERM
 
 #include "scheduler.h"
+#include <unistd.h>
+#include <fcntl.h>
 
 //function that does some work
 void count_down(void * arg);
+void wrap_read_wrap(void * arg);
+ssize_t read_wrap(int fd, void * buf, size_t count);
 
 int main(int argc, char * argv[]){
   scheduler_begin();
+
+  #ifdef TERM
+  int fd = STDIN_FILENO;
+  #else
+  int fd = open("testfile.txt", O_RDONLY);
+  #endif
 
   int n1 = 10, n2 = 9, n3 = 8, n4 = 7, n5 = 6, n6 = 35;
   int n7 = 5, n8 = 4, n9 = 3, n10 = 2, n11 = 1, n12 = 11;
@@ -16,6 +27,7 @@ int main(int argc, char * argv[]){
   thread_fork(count_down, &n1);
   thread_fork(count_down, &n2);
   thread_fork(count_down, &n3);
+  thread_fork(wrap_read_wrap, &fd);
   thread_fork(count_down, &n4);
   thread_fork(count_down, &n5);
   thread_fork(count_down, &n7);
@@ -31,8 +43,46 @@ int main(int argc, char * argv[]){
     return 0;
 }
 
+void wrap_read_wrap(void*arg){
+    printf("Starting wrap_read_wrap\n");
+    int fd = *(int*)arg;
+    int sz;
+
+    #ifndef TERM
+    lseek(fd, 10, SEEK_SET);
+    
+    struct stat stt;
+    if(!stat("testfile.txt", &stt))
+        sz = stt.st_size;
+    else return;
+    char * buf = malloc(sz);
+
+    #else
+    char buf[100];
+    sz = 99;
+    #endif
+
+    int rd = read_wrap(fd, buf, sz);
+
+    #ifndef TERM
+    close(fd);
+    #endif
+
+    printf("Contents: \n");
+    printf("%s", buf);
+    printf("Size of file: \n");
+    printf("%d\n", sz);
+    printf("Return value: \n");
+    printf("%d\n", rd);
+    
+    #ifndef TERM
+    free(buf);
+    #endif
+}
+
 //decrement argument and print its value
 void count_down(void * arg){
+    printf("Counting down from %d\n", *(int*)arg);
     while(*(int*)arg > 0){
         --*(int*)arg;
         printf("%d\n", *(int*)arg);
